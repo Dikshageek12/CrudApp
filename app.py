a457b34a3a1a9c10711551aa8f464a8c3f2587f9
Diksha <dikshasharma8795@gmail.com>
2026-02-05 14:28:50 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///employee.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+db = SQLAlchemy(app)
+app.app_context().push()
+
+class Employee(db.Model):
+    sno = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(200),nullable = False)
+    email = db.Column(db.String(500), nullable = False)
+
+@app.route("/")
+def home():
+    employee = Employee(name = "Diksha Sharma",email = "dikshasharma@gmail.com")
+    db.session.add(employee)
+    db.session.commit()
+    allemployee = Employee.query.all()
+    return render_template('index.html',allemployee = allemployee)
+@app.route("/about")
+def about():
+    return render_template('about.html')
+if __name__ == '__main__':
+    app.run(debug=True)
